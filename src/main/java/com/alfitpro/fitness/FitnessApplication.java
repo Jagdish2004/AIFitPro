6091b2d53ae2e101098a40ec7f7222451488e581
Jagdishraut2004@gmail.com <jagdishraut2004@gmail.com>
2025-07-15 00:59:43 +0530

    initialization of project

---
+++ src/main/java/com/alfitpro/fitness/FitnessApplication.java
@@ -0,0 +1,16 @@
+package com.alfitpro.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FitnessApplication.class, args);
+		System.out.println("Application started!");
+
+	}
+
+}
---
+++ src/test/java/com/alfitpro/fitness/FitnessApplicationTests.java
@@ -0,0 +1,14 @@
+package com.alfitpro.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("This is Test file running.....");
+	}
+
+}
