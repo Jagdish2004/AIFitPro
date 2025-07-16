1570ef99602faf65b251e56b0c1ec608be52c0c4
Jagdishraut2004@gmail.com <jagdishraut2004@gmail.com>
2025-07-17 02:17:39 +0530

    pgSQL db connection and user schema added

---
+++ src/main/java/com/alfitpro/fitness/model/User.java
@@ -0,0 +1,40 @@
+package com.alfitpro.fitness.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    private String FirstName;
+    private String LastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/com/alfitpro/fitness/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.alfitpro.fitness.model;
+
+public enum UserRole {
+    USER, ADMIN
+}
