a63df859e13bf61886bebfb22f27d60f7b61e737
Jagdishraut2004@gmail.com <jagdishraut2004@gmail.com>
2025-08-03 01:27:03 +0530

    microservice3 added for intigreting AI llm for recommendation

---
+++ AiSuggestionService/src/main/java/com/fitness/AiSuggestionService/model/Recommendation.java
@@ -0,0 +1,35 @@
+package com.fitness.AiSuggestionService.model;
+
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.annotation.Collation;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "recommendation")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recommendation{
+
+    @Id
+    private String id;
+
+    private String activityId;
+    private String userId;
+    private String reccommendation;
+    private List<String> improvements;
+    private List<String> suggestions;
+    private List<String> saftey;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
